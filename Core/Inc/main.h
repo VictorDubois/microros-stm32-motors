e05aa33359cc08225b3a6644512b7f162150d122
Victor on ROS2 VM <victor.dubois@free.fr>
2023-12-12 22:06:32 +0100

    add pin names

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -73,7 +73,43 @@ void Error_Handler(void);
 #define SWO_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
-
+#define PWM_MAX 0xFF
+#define ENC2_SIGB_Pin GPIO_PIN_0
+#define ENC2_SIGB_GPIO_Port GPIOC
+#define ENC2_SIGA_Pin GPIO_PIN_1
+#define ENC2_SIGA_GPIO_Port GPIOC
+#define ENC1_SIGA_Pin GPIO_PIN_0
+#define ENC1_SIGA_GPIO_Port GPIOA
+#define ENC1_SIGB_Pin GPIO_PIN_1
+#define ENC1_SIGB_GPIO_Port GPIOA
+#define BRAKE_Pin GPIO_PIN_4
+#define BRAKE_GPIO_Port GPIOA
+#define DIR_B_Pin GPIO_PIN_5
+#define DIR_B_GPIO_Port GPIOA
+#define DIR_A_Pin GPIO_PIN_6
+#define DIR_A_GPIO_Port GPIOA
+#define SPI_CS_Pin GPIO_PIN_7
+#define SPI_CS_GPIO_Port GPIOA
+#define TEMP_ALERT_Pin GPIO_PIN_0
+#define TEMP_ALERT_GPIO_Port GPIOB
+#define PWM_A_Pin GPIO_PIN_1
+#define PWM_A_GPIO_Port GPIOB
+#define PWM_A_passive_Pin GPIO_PIN_10
+#define PWM_A_passive_GPIO_Port GPIOB
+#define SPI_MISO_Pin GPIO_PIN_7
+#define SPI_MISO_GPIO_Port GPIOC
+#define SPI_CLK_Pin GPIO_PIN_9
+#define SPI_CLK_GPIO_Port GPIOA
+#define ENC1_SIGA_PASSIVE_Pin GPIO_PIN_10
+#define ENC1_SIGA_PASSIVE_GPIO_Port GPIOA
+#define DBG2_Pin GPIO_PIN_15
+#define DBG2_GPIO_Port GPIOA
+#define PWM_B_Pin GPIO_PIN_4
+#define PWM_B_GPIO_Port GPIOB
+#define SPI_MOSI_Pin GPIO_PIN_6
+#define SPI_MOSI_GPIO_Port GPIOB
+#define DBG1_Pin GPIO_PIN_7
+#define DBG1_GPIO_Port GPIOB
 /* USER CODE END Private defines */
 
 #ifdef __cplusplus
